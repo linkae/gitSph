9487887fbaa93f1589e8b9d7d7fb462c7f45c77f
muzix <49109505+linkae@users.noreply.github.com>
2023-01-30 11:59:01 +0800

    Add files via upload

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <!-- 三级联动全局组件，已经注册为全局组件，因此不需要再引入 -->
+    <type-nav />
+    <list-container />
+    <Recommend />
+    <Rank />
+    <Like />
+    <!-- 利用 props 给 floor 子组件传递数据 -->
+    <Floor v-for="floor in floorList" :key="floor.id" :list="floor" />
+    <Brand />
+  </div>
+</template>
+
+<script>
+// 引入其余组件
+import ListContainer from "./ListContainer";
+import Recommend from "./Recommend";
+import Rank from "./Rank";
+import Like from "./Like";
+import Floor from "./Floor";
+import Brand from "./Brand";
+import { mapState } from "vuex";
+
+export default {
+  name: "Home",
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  },
+  mounted() {
+    this.$store.dispatch("getFloorList");
+  },
+  computed: {
+    ...mapState({
+      floorList: (state) => state.home.floorList,
+    }),
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/PaySuccess/index.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="paysuccess">
+    <div class="success">
+      <h3>
+        <img src="./images/right.png" width="48" height="48" />
+        恭喜您，支付成功啦！
+      </h3>
+      <div class="paydetail">
+        <p class="button">
+          <router-link class="btn-look" to="/center">查看订单</router-link>
+          <router-link class="btn-goshop" to="/">继续购物</router-link>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaySuccess",
+  beforeRouteEnter(to, from, next) {
+    // 在渲染组件的对应路由被 confirm 前调用
+    // 不能获取组件实例 this
+    if (from.path == "/pay") {
+      next();
+    } else {
+      next();
+    }
+  },
+  // beforeRouteUpdate(to, from, next) {
+  // 在当前路由改变，但是该组件被复用时调用
+  // 例：对于一个带有动态参数的路径 /foo/:id, 在 /foo/1 和 /foo/2 之间跳转的时候
+  // 由于会渲染同样的 Foo 组件，因此组件实例会被复用，因而这个钩子在这个情况下会被调用
+  // 可以访问 this
+  // },
+};
+</script>
+
+<style lang="less" scoped>
+.paysuccess {
+  margin: 20px auto;
+  padding: 25px;
+  border: 1px solid rgb(211, 211, 211);
+  width: 1200px;
+  width: 1148px;
+
+  .success {
+    width: 500px;
+    margin: 0 auto;
+
+    h3 {
+      margin: 20px 0;
+      font-weight: 700;
+      font-size: 20px;
+      line-height: 30px;
+
+      img {
+        max-width: 100%;
+        vertical-align: middle;
+        border: 0;
+        margin-right: 14px;
+      }
+    }
+
+    .paydetail {
+      margin-left: 66px;
+      font-size: 15px;
+
+      .button {
+        margin: 30px 0;
+        line-height: 26px;
+
+        a {
+          display: inline-block;
+          box-sizing: border-box;
+          text-align: center;
+          vertical-align: middle;
+          cursor: pointer;
+          border-radius: 2px;
+          user-select: none;
+          font-size: 18px;
+          padding: 4px 20px;
+          line-height: 22px;
+          text-decoration: none;
+
+          &.btn-look {
+            margin-right: 13px;
+            color: #fff;
+            background-color: #e1251b;
+            border: 1px solid #e1251b;
+          }
+
+          &.btn-goshop {
+            color: #666;
+            background-color: #eee;
+            border: 1px solid #e1e1e1;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
